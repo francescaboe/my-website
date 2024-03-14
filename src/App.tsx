aa9df6e4a163f86f1415da1a258cea42a87538e0
francescaboe <francesca.boe.ac@gmail.com>
2024-03-14 11:31:46 +0100

    add rock paper scissors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import RockPaperScissors from 'components/rock-paper-scissors';
 
 const App = () => {
-    return <div>Hello, world!</div>;
+  return (
+    <div>
+      Hello, world!
+      <RockPaperScissors />
+    </div>
+  );
 };
 export default App;
---
+++ src/components/rock-paper-scissors/index.ts
@@ -0,0 +1,1 @@
+export { default } from './rock-paper-scissors';
