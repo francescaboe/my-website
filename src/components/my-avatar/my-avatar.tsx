856c24bcd4f1fd6bbe547d58bc97f977e98de57c
francescaboe <francesca.boe.ac@gmail.com>
2024-03-17 18:02:45 +0100

    add my css avatar

---
+++ src/components/my-avatar/my-avatar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './my-avatar.css';
+function MyAvatar() {
+  return (
+    <div className="my-avatar-container">
+      <div className="my-hair"></div>
+      <div className="my-ear"></div>
+      <div className="my-face">
+        <div className="my-brows">
+          <div className="my-brow"></div>
+          <div className="my-brow"></div>
+        </div>
+        <div className="my-eyes">
+          <div className="my-eye"></div>
+          <div className="my-eye"></div>
+        </div>
+        <div className="my-nose"></div>
+        <div className="my-mouth"></div>
+        <div className="my-body"></div>
+      </div>
+    </div>
+  );
+}
+
+export default MyAvatar;
