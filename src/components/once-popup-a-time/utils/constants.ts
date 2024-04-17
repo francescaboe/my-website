d4fd3e69eb3b4112ad0a9a3bcc330c068e73e7dd
francescaboe <francesca.boe.ac@gmail.com>
2024-04-17 14:58:55 +0200

    add story constants to opat

---
+++ src/components/once-popup-a-time/utils/constants.ts
@@ -0,0 +1,30 @@
+export const STORY: { title: string; content: string }[] = [
+  {
+    title: 'Welcome to My Forest',
+    content: 'Hover on the image to see animation. \nPress next to turn the page',
+  },
+  {
+    title: 'The Enchanted Forest',
+    content:
+      'In the heart of an enchanted forest, a lonely princess awaits, surrounded by the whispering trees',
+  },
+  {
+    title: 'A Fiery Encounter',
+    content:
+      "Suddenly, a fierce dragon emerges, its scales reflecting the glint of danger as it blocks the princess's path",
+  },
+  {
+    title: 'Unexpected Hero',
+    content:
+      'A valiant prince appears, sword drawn, determined to rescue the princess from the looming threat of the dragon',
+  },
+  {
+    title: 'A Twisted Union',
+    content:
+      'But as the prince approaches, the princess reveals a dark secret: she desires no rescue. In a shocking twist, she turns the tables, vanquishing the prince and choosing an new path—a union with the dragon.',
+  },
+  {
+    title: 'THE END',
+    content: '...',
+  },
+];
