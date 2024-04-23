4462e2ee9ecb97db4f1e78c60c8664701abfdefc
francescaboe <francesca.boe.ac@gmail.com>
2024-04-23 17:05:47 +0200

    move experiments around

--- src/App.tsx
+++ src/App.tsx
@@ -4,48 +4,49 @@ import PlayRps from 'components/play-rps';
 import OncePopupATime from 'components/once-popup-a-time';
 const App = () => {
   return (
-    <div className="flex p-6 font-mono">
+    <div className="flex p-6 font-mono h-full">
       <div className="flex-auto">
         {/*HEADER*/}
         <div className="relative flex flex-wrap items-baseline pb-6 before:bg-black before:absolute before:-top-6 before:bottom-0 before:-left-60 before:-right-6">
           <h1 className="relative w-full flex-none mb-2 text-2xl font-semibold text-white">
-            landscape css
+            {/*add tailwind for landscape*/}
           </h1>
         </div>
         <div className="absolute top-5">
           <MyAvatar />
         </div>
         {/*ABOUT*/}
-        <div className="flex items-baseline my-6 border-b-4">
-          <h1 className="font-bold uppercase">About</h1>
+        <div className="flex items-baseline my-6 border-b-4 mt-20">
+          <h1 className="font-bold uppercase text-4xl md:text-2xl">About</h1>
         </div>
-        <p>
+        <p className="text-4xl md:text-2xl">
           Hey, I'm Francesca. I'm a frontend developer based in Berlin. I build stuff with React.
           Buzzwords I'm into include: Typescript, Tailwind, continuous learning, linting [todo:
           accessibility, testing, figma, ux]
         </p>
         {/*EXPERIMENTS*/}
         <div className="flex items-baseline my-6 border-b-4">
-          <h1 className="font-bold uppercase">Sometimes inappropriate usages of code</h1>
+          <h1 className="font-bold uppercase text-4xl md:text-2xl">Inappropriate use of code</h1>
         </div>
         <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
           <button className="px-6 h-12 uppercase font-semibold tracking-wider border border-slate-200 bg-amber-200 text-slate-900">
-            Experiment 1: Rock, Paper, Scissors
-          </button>
-          <button className="px-6 h-12 uppercase font-semibold tracking-wider border border-slate-200 bg-amber-700 text-slate-900">
-            Experiment 2: Once popup a time with Tailwind
+            Rock, Paper, Scissors
           </button>
         </div>
         {/*TODO: add as accordion?*/}
         {/*PLAY*/}
-        <div className="flex items-baseline justify-center my-6">{/*<PlayRps />*/}</div>
+        <div className="flex items-baseline justify-center my-6">
+          <PlayRps />
+        </div>
         {/*ONCE POPUP*/}
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <button className="px-6 h-12 uppercase font-semibold tracking-wider border border-slate-200 bg-amber-700 text-slate-900">
+            Once popup a time with Tailwind
+          </button>
+        </div>
         <div className="flex items-baseline justify-center my-6">
           <OncePopupATime />
         </div>
-        {/*<div className="flex items-baseline justify-center my-6">
-          <PlayRps />
-        </div>*/}
         {/*LIKE BUTTON*/}
         {/*<button
             className="flex-none flex items-center justify-center w-12 h-12 text-black"
