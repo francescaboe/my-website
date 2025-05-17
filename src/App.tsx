180101385dfe7b4619417a1db5ddd0275a58e239
francescaboe <francesca.boe.ac@gmail.com>
2025-05-17 18:14:21 +0200

    try to center

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MyAvatar from 'components/my-avatar';
 const App = () => {
   return (
-    <div className="flex justify-center items-center p-6 font-mono h-dvh bg-amber-400">
+    <div className="flex flex-col gap-4 justify-center items-center h-screen m-auto">
       <MyAvatar />
     </div>
   );
