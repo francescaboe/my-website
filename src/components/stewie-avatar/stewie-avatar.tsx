a7be55ebcb0aa549b804f339d09d3a1d9750695f
francescaboe <francesca.boe.ac@gmail.com>
2025-07-15 16:20:14 +0200

    start stewie

--- src/components/stewie-avatar/stewie-avatar.tsx
+++ src/components/stewie-avatar/stewie-avatar.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import './stewie-avatar.css';
 
 function StewieAvatar() {
-  return <div className="avatar-container stewie-avatar-container">yo</div>;
+  return (
+    <div className="avatar-container stewie-avatar-container">
+      <div className="stewie-avatar-hair"></div>
+      <div className="stewie-avatar-head">
+        <div className="stewie-avatar-brows"></div>
+        <div className="stewie-avatar-eyes">
+          <div className="stewie-avatar-eye"></div>
+          <div className="stewie-avatar-eye"></div>
+        </div>
+        <div className="stewie-avatar-nose"></div>
+        <div className="stewie-avatar-mouth"></div>
+      </div>
+      <div className="stewie-avatar-body"></div>
+    </div>
+  );
 }
 
 export default StewieAvatar;
